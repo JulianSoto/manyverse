c9bcd3f363f155d3d31f532823edbc0be2d8ccac
Jacob Karlsson <jacob.karlsson95@gmail.com>
2021-01-04 21:18:09 +0100

    Start installing react-native-web

--- src/desktop/index.ts
+++ src/desktop/index.ts
@@ -1,11 +1,11 @@
-/* Copyright (C) 2018-2020 The Manyverse Authors.
+/* Copyright (C) 2018-2021 The Manyverse Authors.
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import {run} from '@cycle/run';
-import {makeDOMDriver, div, h1, button} from '@cycle/react-dom';
+import {makeReactNativeDriver, View, Text, Button} from '@cycle/react-native';
 import makeClient from './ssb/client';
 const pull = require('pull-stream');
 
@@ -16,7 +16,7 @@ function main(sources: any) {
   const count$ = inc$.fold((count: number) => count + 1, 0);
 
   const vdom$ = count$.map((i: number) =>
-    div([h1(`Counter: ${i}`), button(inc, 'Increment')]),
+    View([Text(`Counter: ${i}`), Button(inc, 'Increment')]),
   );
 
   return {
@@ -25,7 +25,7 @@ function main(sources: any) {
 }
 
 run(main, {
-  react: makeDOMDriver(document.getElementById('app')),
+  react: makeReactNativeDriver('Desktop Manyverse'),
 });
 
 function myapp() {
